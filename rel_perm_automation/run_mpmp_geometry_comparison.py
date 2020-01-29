7aae9f1fe533fa8c80444a72903c0c43d658bba6
alexgigliotti <alex.gigliotti@utexas.edu>
2020-01-29 11:00:16 -0600

    add general output directory

--- rel_perm_automation/run_mpmp_geometry_comparison.py
+++ rel_perm_automation/run_mpmp_geometry_comparison.py
@@ -18,12 +18,12 @@ class AutomatedMPMP():
         self.geometry_links = names_and_links_data[:][:, 1]
 
         # Set output directory
-        self.output_directory = 'geometry_comparison_test'
+        self.output_directory = '/storage/gigliotti/geometry_comparison_test'
 
         # Set simulation size
-        nx = 305
-        ny = 300
-        nz = 300
+        nx = 205
+        ny = 200
+        nz = 200
         self.simulation_size = [nx, ny, nz]
 
         return
@@ -41,25 +41,43 @@ class AutomatedMPMP():
         geom_names = self.geometry_names
         output_dir = self.output_directory
         
-        os.system("mkdir ../" + output_dir + "/" + geom_names[i])
+        os.system("mkdir " + output_dir)  # create parent output directory
+        os.system("mkdir " + output_dir + "/" + geom_names[i])  # create output for geometry
+        os.system("mkdir " + output_dir + "/tmp")  # create output for simulation files if not there
+        
+        os.system("mkdir " + output_dir + "/tmp/2_phase_shanchen_output")
+        os.system("mkdir " + output_dir + "/tmp/2_phase_shanchen_output/fluid_geometry_files")
+        os.system("mkdir " + output_dir + "/tmp/2_phase_shanchen_output/fluid_geometry_files/input")
+        os.system("mkdir " + output_dir + "/tmp/2_phase_shanchen_output/gifs")
+        os.system("mkdir " + output_dir + "/tmp/2_phase_shanchen_output/numerical_outputs")
+
+        os.system("mkdir " + output_dir + "/tmp/raw_geometry")
+
+        os.system("mkdir " + output_dir + "/tmp/rel_perm_calculation")
+        os.system("mkdir " + output_dir + "/tmp/rel_perm_calculation/fluid_geometry_input")
+        os.system("mkdir " + output_dir + "/tmp/rel_perm_calculation/fluid_geometry_input/input")
+        os.system("mkdir " + output_dir + "/tmp/rel_perm_calculation/output")
+        os.system("mkdir " + output_dir + "/tmp/rel_perm_calculation/numerical_outputs")
         
         return
     
     def download_geometry_file(self, i):
         geom_names = self.geometry_names
         geom_links = self.geometry_links
+        output_dir = self.output_directory
         
-        os.system("wget -O tmp/raw_geometry/" + geom_names[i] + ".raw " + geom_links[i])
+        os.system("wget -O " + output_dir + "/tmp/raw_geometry/" + geom_names[i] + ".raw " + geom_links[i])
         
         return 
     
     def update_and_run_geometry_creation_matlab_file(self, i):
         geom_names = self.geometry_names
         Nx = self.simulation_size[0]
+        output_dir = self.output_directory
        
         file_to_edit = "create_geometry.m"
         line_to_find_and_replace = 'raw_geometry_file_to_open ='
-        replacement_line = "raw_geometry_file_to_open = 'tmp/raw_geometry/" + geom_names[i] + ".raw';"
+        replacement_line = "raw_geometry_file_to_open = '" + output_dir + "/tmp/raw_geometry/" + geom_names[i] + ".raw';"
         self.replace_line_in_file(file_to_edit, line_to_find_and_replace, replacement_line)
 
         line_to_find_and_replace = 'geometry_file_output_name ='
@@ -97,14 +115,14 @@ class AutomatedMPMP():
         return
  
     def run_shan_chen_lbm_simulation(self):
-
+        output_dir = self.output_directory
         os.system('bash 2_phase_auto.sh')
 
         # Organize outputs
         os.system('mv capillary_number_data.csv capillary_pressure_data.csv mobility_ratio_data.csv '\
-                  'run_data.csv runnum.dat tmp/2_phase_shanchen_output/numerical_outputs')
-        os.system('mv tmp/2_phase_shanchen_output/rho_f1_*.gif tmp/2_phase_shanchen_output/gifs/')
-        os.system('mv tmp/2_phase_shanchen_output/rho_f1_*.vti tmp/2_phase_shanchen_output/fluid_geometry_files/')
+                  'run_data.csv runnum.dat ' + output_dir + '/tmp/2_phase_shanchen_output/numerical_outputs')
+        os.system('mv ' + output_dir + '/tmp/2_phase_shanchen_output/rho_f1_*.gif ' + output_dir + '/tmp/2_phase_shanchen_output/gifs/')
+        os.system('mv ' + output_dir + '/tmp/2_phase_shanchen_output/rho_f1_*.vti ' + output_dir + '/tmp/2_phase_shanchen_output/fluid_geometry_files/')
 
         return
 
@@ -112,6 +130,7 @@ class AutomatedMPMP():
 
         geom_names = self.geometry_names
         sim_size = self.simulation_size
+        output_dir = self.output_directory
 
         # Update file name
         file_to_edit = 'rel_perm_input.xml'
@@ -129,7 +148,7 @@ class AutomatedMPMP():
         self.replace_line_in_file(file_to_edit, line_to_find_and_replace, replacement_line)
         
         # Get number of runs from Shan Chen Output. Multiply by 2 for wetting and non-wetting phases
-        number_of_runs = np.loadtxt('tmp/2_phase_shanchen_output/numerical_outputs/runnum.dat')
+        number_of_runs = np.loadtxt(output_dir + '/tmp/2_phase_shanchen_output/numerical_outputs/runnum.dat')
         self.number_of_runs = number_of_runs
         file_to_edit = 'rel_perm_input.xml'
         line_to_find_and_replace = '    <num>'
@@ -138,28 +157,35 @@ class AutomatedMPMP():
 
         # Copy geometry file to fluid gemoetry directory per rel perm c++ file requirements
         # os.system("input/" + geom_names[i] + "* " + "tmp/2_phase_shanchen_output/fluid_geometry_files")
-        os.system("cp input/*.dat tmp/rel_perm_calculation/fluid_geometry_input/input")
+        os.system("cp input/*.dat "  + output_dir + "/tmp/rel_perm_calculation/fluid_geometry_input/input")
         # os.system("cp input/*.dat tmp/rel_perm_output")
 
         return
 
     def create_geometry_for_rel_perms(self):
-
+        output_dir = self.output_directory
         # Generate geometry files for rel perm calculation
-        add_path = "addpath('/workspace/gigliotti/MultiphasePorousMediaPalabos/post-processing');"
+
+
+
+        #### MAKE SURE TO CHANGE MPMP_fork TO ACTUAL NAME LATER!!!
+        
+
+
+        add_path = "addpath('/workspace/gigliotti/MultiphasePorousMediaPalabos_fork/post-processing');"
         os.system('matlab -r "' + add_path +' read_save_fluids; exit"')
 
         # Organize output
-        os.system('mv tmp/2_phase_shanchen_output/fluid_geometry_files/*.csv tmp/2_phase_shanchen_output/fluid_geometry_files/Sw.csv')
-        os.system('mv lattice_f* tmp/rel_perm_calculation/fluid_geometry_input/')
-        os.system('mv tmp/2_phase_shanchen_output/fluid_geometry_files/Sw.csv tmp/2_phase_shanchen_output/numerical_outputs/')
+        os.system('mv ' + output_dir +  'tmp/2_phase_shanchen_output/fluid_geometry_files/*.csv ' + output_dir + '/tmp/2_phase_shanchen_output/fluid_geometry_files/Sw.csv')
+        os.system('mv lattice_f* ' + output_dir +  '/tmp/rel_perm_calculation/fluid_geometry_input/')
+        os.system('mv ' + output_dir + '/tmp/2_phase_shanchen_output/fluid_geometry_files/Sw.csv ' + output_dir +  '/tmp/2_phase_shanchen_output/numerical_outputs/')
 
         return
 
     def run_rel_perm_simulation(self):
 
         os.system('bash rel_perm_auto.sh')
-        os.system('mv perm_data.csv relperm_data.csv tmp/rel_perm_calculation/numerical_outputs/')
+        os.system('mv perm_data.csv relperm_data.csv ' + output_dir + '/tmp/rel_perm_calculation/numerical_outputs/')
 
         return
 
@@ -169,10 +195,10 @@ class AutomatedMPMP():
         output_dir = self.output_directory
 
         # Copy all results to the output directory for the geometry being simulated
-        os.system("cp -a tmp/. ../" + output_dir + "/" + geom_names[i] + "/")
+        os.system("cp -a " + output_dir + "/tmp/. " + output_dir + "/" + geom_names[i] + "/")
 
         # Delete temporary files and geometry input file in automation directory
-        os.system("find tmp/ -type f -delete")
+        os.system("find " + output_dir + "/tmp/ -type f -delete")
         os.system("find input/ -type f -delete")
 
         return
@@ -182,8 +208,8 @@ class AutomatedMPMP():
         geom_names = self.geometry_names
         output_dir = self.output_directory
 
-        shan_chen_outputs_dir = "../" + output_dir + "/" + geom_names[i] + "/2_phase_shanchen_output/numerical_outputs/"
-        rel_perm_outputs_dir = "../" + output_dir + "/" + geom_names[i] + "/rel_perm_calculation/numerical_outputs/"
+        shan_chen_outputs_dir = output_dir + "/" + geom_names[i] + "/2_phase_shanchen_output/numerical_outputs/"
+        rel_perm_outputs_dir = output_dir + "/" + geom_names[i] + "/rel_perm_calculation/numerical_outputs/"
 
         number_of_runs = int(np.loadtxt(shan_chen_outputs_dir + 'runnum.dat'))
         self.number_of_runs = number_of_runs
@@ -221,7 +247,7 @@ class AutomatedMPMP():
         krnw = self.relative_permeability_nonwetting
 
         # Create output directory for figures
-        figures_dir = "../" + output_dir + "/" + geom_names[i] + "/figures"
+        figures_dir = output_dir + "/" + geom_names[i] + "/figures"
         os.system("mkdir " + figures_dir)
         
         # Replace underscores in names with a space so it looks nice
@@ -275,31 +301,31 @@ for i in range(len(mpmp.geometry_names)):
     mpmp.download_geometry_file(i)
     
     # 3) Update file names in matlab file
-    mpmp.update_and_run_geometry_creation_matlab_file(i)
+    # mpmp.update_and_run_geometry_creation_matlab_file(i)
     
     # 4) Edit xml input for Shan Chen
-    mpmp.update_2_phase_shan_chen_input_file(i)
+    # mpmp.update_2_phase_shan_chen_input_file(i)
 
     # 5) Run Shan Chen
-    mpmp.run_shan_chen_lbm_simulation()
+    # mpmp.run_shan_chen_lbm_simulation()
  
     # 6) Update xml input file for rel perms
-    mpmp.update_rel_perm_input_file(i)
+    # mpmp.update_rel_perm_input_file(i)
 
     # 7) Generate geometries for rel perms
-    mpmp.create_geometry_for_rel_perms()    
+    # mpmp.create_geometry_for_rel_perms()    
 
     # 8) Run Rel Perm simulation
-    mpmp.run_rel_perm_simulation()
+    # mpmp.run_rel_perm_simulation()
 
     # 9) Save results and clear tmp/ for next simulation
-    mpmp.save_results_and_clear_temp(i)
+    # mpmp.save_results_and_clear_temp(i)
 
     # 10) Parse data and create plots
-    mpmp.parse_simulation_data(i)
-    mpmp.plot_results(i)
+    # mpmp.parse_simulation_data(i)
+    # mpmp.plot_results(i)
 
     # 11) Email when done with the time!
     end_time = datetime.now() - start_time
-    mpmp.email_when_done(i, end_time)
+    # mpmp.email_when_done(i, end_time)
 
